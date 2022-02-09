e5239e5f6e55e8fc23e0f44d6884c47929d9ddd5
yanggang <yangglive@icloud.com>
2022-02-09 15:58:23 +0800

    change

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"crypto/x509"
+	"encoding/json"
+	"encoding/pem"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello test")
+	// fmt.Println("hello test")
+	// r := net.Resolver{}
+	// ips, _ := r.LookupTXT(context.TODO(), "_dnsauth.dev.x-t.top")
+	// fmt.Println(ips)
+	certPEMBlock, err := os.ReadFile("/Users/yanggang/Documents/cert/teatak/teatak.pem")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	block, _ := pem.Decode(certPEMBlock)
+
+	cert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		panic(err)
+	}
+
+	data, _ := json.Marshal(cert)
+	fmt.Println(string(data))
+
 }
